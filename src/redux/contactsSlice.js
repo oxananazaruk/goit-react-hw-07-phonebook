0548469792fe4bee862f8bd400b0ca2d84961ba5
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-12-10 17:57:18 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -25,14 +25,14 @@ const contactSlice = createSlice({
         state.error = null;
         state.items = action.payload;
       })
-      .addCase(addContact.rejected, handleRejected)
+      .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items.push(action.payload);
       })
-      .addCase(deleteContact.rejected, handleRejected)
+      .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.isLoading = false;
@@ -41,7 +41,8 @@ const contactSlice = createSlice({
           contact => contact.id === action.payload.id
         );
         state.items.splice(index, 1);
-      });
+      })
+      .addCase(deleteContact.rejected, handleRejected);
   },
 });
 
